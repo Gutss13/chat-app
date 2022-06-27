488688f5433d346842ff548cc2d8cfa5ea2abeb9
Gutss13 <gegimargiani13@gmail.com>
2022-06-27 23:03:10 +0400

    code

--- server.js
+++ server.js
@@ -21,12 +21,12 @@ db.once('open', () => console.log('Connected to Database'));
 
 app.use(express.json());
 
-const chatRouter = require('./routes/router');
+const chatRouter = require('routes/router');
 const wss = new WebSocket.Server({ server });
 
 app.use(express.static(path.resolve(__dirname, './client/build')));
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
 });
 
 app.use('/', cors(corsOptions), chatRouter);
