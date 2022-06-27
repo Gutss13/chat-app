cdab55bc084de3fe49dfe92f74d4bc36743f3aef
Gutss13 <gegimargiani13@gmail.com>
2022-06-28 01:32:44 +0400

    code

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-require('dotenv').config();
+const dotenv = require('dotenv');
+dotenv.config();
+
 const axios = require('axios');
 const cors = require('cors');
 const express = require('express');
