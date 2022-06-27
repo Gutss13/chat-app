20c257509b759b440f81496bc312273d0ab09d93
Gutss13 <gegimargiani13@gmail.com>
2022-06-27 23:38:01 +0400

    code

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const wss = new WebSocket.Server({ server });
 
 app.use(express.static(path.resolve(__dirname, './client/build')));
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
 });
 
 app.use('/', cors(corsOptions), chatRouter);
