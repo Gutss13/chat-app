cc20d449debe119e8ee1e302b43d1c1a9342bbfa
Gutss13 <gegimargiani13@gmail.com>
2022-06-28 02:04:06 +0400

    code

--- server.js
+++ server.js
@@ -16,13 +16,10 @@ var corsOptions = {
   optionsSuccessStatus: 200,
 };
 
-mongoose.connect(
-  'mongodb+srv://Guts:598969209@nodeexpressprojects.7o2lc.mongodb.net/?retryWrites=true',
-  {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-  }
-);
+mongoose.connect('process.env.MONGODB_URI', {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to Database'));
