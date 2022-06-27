9b684bd9f6d37183c1975357346cbd1f4de7517b
Gutss13 <gegimargiani13@gmail.com>
2022-06-28 02:36:30 +0400

    code

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var corsOptions = {
   optionsSuccessStatus: 200,
 };
 
-mongoose.connect('process.env.MONGODB_URI');
+mongoose.connect('process.env.MONGODB_URI'{ useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to Database'));
