802b0e764d962cbd95287274499efc7298e1dbb7
Gutss13 <gegimargiani13@gmail.com>
2022-06-29 16:42:00 +0400

    code

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const chatRouter = require('./routes/router');
 const wss = new WebSocket.Server({ server });
 
 app.use(express.static(path.resolve(__dirname, 'client/build')));
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
 });
 
